b10fbb7a3c5e0983ed83f29ead7942588efea1b8
NicolasOsorio2912 <micorreo@github.comn.osoriop@uniandes.edu.co>
2026-02-13 16:14:08 -0500

    'basic queue functions'

--- DataStructures/Queue/queue.py
+++ DataStructures/Queue/queue.py
@@ -2,20 +2,23 @@ from DataStructures.List import single_linked_list as sll
 
 def new_queue():
     nq = {
-        'elements': None,
+        'elements': sll.new_list(),
         'size': 0
     }
     return nq
 def enqueue(my_queue, element):
-    return my_queue['elements'].sll.add_last(element)
+    sll.add_last(my_queue['elements'], element)
+    my_queue['size'] += 1
+    return my_queue
 
 def dequeue(my_queue):
     element = my_queue['elements']['first']
-    my_queue['elements'].sll.remove_first()
+    sll.remove_first(my_queue['elements'])
+    my_queue['size'] -= 1
     return element
 
-def peek():
-    pass
+def peek(my_queue):
+    return sll.first_element(my_queue['elements'])
 
 def size(my_queue):
     return my_queue['size']
